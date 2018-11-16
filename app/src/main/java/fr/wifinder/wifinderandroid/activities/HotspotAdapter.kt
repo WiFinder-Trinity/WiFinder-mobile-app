0fe13b52ab6c932a22db61090da98c3a5e7c3807
Quentin JODER <quentinjoder@gmail.com>
2018-11-16 12:12:29 +0100

    Should display hotspots list (but does not work)

--- app/src/main/java/fr/wifinder/wifinderandroid/activities/HotspotAdapter.kt
+++ app/src/main/java/fr/wifinder/wifinderandroid/activities/HotspotAdapter.kt
@@ -1,5 +1,6 @@
 package fr.wifinder.wifinderandroid.activities
 
+import android.net.wifi.ScanResult
 import android.support.v7.app.AlertDialog
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
@@ -8,20 +9,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import fr.wifinder_trinity.wifinderandroid.R
 
-class HotspotAdapter : RecyclerView.Adapter<HotspotAdapter.MyViewHolder>() {
-
-    private val characters = arrayOf(
-            Pair("Lyra Belacqua", "Brave, curious, and crafty, she has been prophesied by the witches to help the balance of life"),
-            Pair("Pantalaimon", "Lyra's daemon, nicknamed Pan."),
-            Pair("Roger Parslow", "Lyra's friends"),
-            Pair("Lord Asriel", "Lyra's uncle"),
-            Pair("Marisa Coulter", "Intelligent and beautiful, but extremely ruthless and callous."),
-            Pair("Iorek Byrnison", "Armoured bear, Rightful king of the panserbjørne. Reduced to a slave of the human village Trollesund."),
-            Pair("Serafina Pekkala", "Witch who closely follows Lyra on her travels."),
-            Pair("Lee Scoresby", "Texan aeronaut who transports Lyra in his balloon. Good friend with Iorek Byrnison."),
-            Pair("Ma Costa", "Gyptian woman whose son, Billy Costa is abducted by the \"Gobblers\"."),
-            Pair("John Faa", "The King of all gyptian people.")
-    )
+class HotspotAdapter (private val hotspots: List<ScanResult>) : RecyclerView.Adapter<HotspotAdapter.MyViewHolder>() {
 
     // Create new views (invoked by the layout manager)
     override fun onCreateViewHolder(parent: ViewGroup,
@@ -34,13 +22,12 @@ class HotspotAdapter : RecyclerView.Adapter<HotspotAdapter.MyViewHolder>() {
 
     // Replace the contents of a view (invoked by the layout manager)
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        val pair = characters.get(position)
+        val pair = Pair(hotspots[position].BSSID, hotspots[position].SSID)
         holder.display(pair)
     }
 
     // Return the size of your dataset (invoked by the layout manager)
-    override fun getItemCount() = characters.size
-
+    override fun getItemCount() = hotspots.size
     inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
         private val name: TextView = itemView.findViewById(R.id.name)
--- app/src/main/java/fr/wifinder/wifinderandroid/activities/HotspotsListActivity.kt
+++ app/src/main/java/fr/wifinder/wifinderandroid/activities/HotspotsListActivity.kt
@@ -1,40 +1,84 @@
 package fr.wifinder.wifinderandroid.activities
 
-import android.os.AsyncTask
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.content.pm.PackageManager
+import android.net.wifi.WifiManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
-import android.util.Log
-import android.webkit.WebView
-import android.webkit.WebViewClient
-import android.widget.Button
-import fr.wifinder.wifinderandroid.apis.CrowdsensingControllerApi
+import fr.wifinder.wifinderandroid.services.PermissionsService
 import fr.wifinder_trinity.wifinderandroid.R
 import kotlinx.android.synthetic.main.activity_hotspots_list.*
-import java.lang.ref.WeakReference
-import java.util.Arrays.asList
+import java.util.*
 
-
-
+/* TODO: Documentation officielle https://developer.android.com/guide/topics/connectivity/wifi-scan#kotlin*/
 class HotspotsListActivity : AppCompatActivity() {
 
     private lateinit var recyclerView: RecyclerView
     private lateinit var viewAdapter: RecyclerView.Adapter<*>
     private lateinit var viewManager: RecyclerView.LayoutManager
 
+    private val ps = PermissionsService()
+
+    private lateinit var wifiManager: WifiManager
+    private var permissionsGranted = false
+
+    private val wifiScanReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            val success = intent.getBooleanExtra(WifiManager.EXTRA_RESULTS_UPDATED, false)
+            if (!success) {
+                textView.text = "Failure: " + Date().toString() + wifiManager.scanResults.size.toString() +" hotspots"
+            } else{
+                textView.text = "Success:" +Date().toString() +wifiManager.scanResults.size.toString()+ "hotspots"
+            }
+            // TODO: reload hotspots list
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hotspots_list)
-        viewManager = LinearLayoutManager(this)
-        viewAdapter = HotspotAdapter()
 
-        recyclerView = findViewById<RecyclerView>(R.id.recyclerView).apply {
-            // use this setting to improve performance if you know that changes
-            // in content do not change the layout size of the RecyclerView
-            /* setHasFixedSize(true) */
-            layoutManager = viewManager
-            adapter = viewAdapter
+        permissionsGranted = ps.getHotspotsListPermission(this)
+        if(permissionsGranted) {
+            wifiManager = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+            val intentFilter = IntentFilter()
+            intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION)
+            baseContext.registerReceiver(wifiScanReceiver, intentFilter)
+            wifiManager.startScan()
+
+            textView.text = "INIT: "+ wifiManager.scanResults.size.toString()
+
+            viewManager = LinearLayoutManager(this)
+            viewAdapter = HotspotAdapter(wifiManager.scanResults)
+            recyclerView = findViewById<RecyclerView>(R.id.recyclerView).apply {
+                // use this setting to improve performance if you know that changes
+                // in content do not change the layout size of the RecyclerView
+                /* setHasFixedSize(true) */
+                layoutManager = viewManager
+                adapter = viewAdapter
+            }
+        }
+    }
+
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>,
+                                            grantResults: IntArray) {
+        permissionsGranted = false
+        when (requestCode) {
+            1 -> {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    permissionsGranted = true
+                }
+            }
         }
     }
 
